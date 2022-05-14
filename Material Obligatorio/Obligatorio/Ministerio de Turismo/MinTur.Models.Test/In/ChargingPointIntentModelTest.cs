8837c39468695dab1e9171c8f1ac2b7f641852c5
Francisco Rossi <65981860+FranRossi@users.noreply.github.com>
2022-05-14 20:32:23 -0300

    Create ChargingPointModel test to recieve through WebApi layer

---
+++ Material Obligatorio/Obligatorio/Ministerio de Turismo/MinTur.Models.Test/In/ChargingPointIntentModelTest.cs
@@ -0,0 +1,30 @@
+﻿using MinTur.Domain.BusinessEntities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MinTur.Models.In;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MinTur.Models.Test.In
+{
+    [TestClass]
+    public class ChargingPointIntentModelTest
+    {
+        [TestMethod]
+        public void ToEntityReturnsAsExpected()
+        {
+            ChargingPointIntentModel chargingPointIntentModel = new ChargingPointIntentModel()
+            {
+                Name = "Punta del este",
+                Description = "Descripyion...",
+                RegionId = 3,
+                Address = "jsafigrehgeruhwdjiw",
+            };
+            ChargingPoint chargingPoint = chargingPointIntentModel.ToEntity();
+
+            Assert.AreEqual(chargingPointIntentModel.Name, chargingPoint.Name);
+            Assert.AreEqual(chargingPointIntentModel.Description, chargingPoint.Description);
+            Assert.AreEqual(chargingPointIntentModel.RegionId, chargingPoint.RegionId);
+            Assert.AreEqual(chargingPointIntentModel.Address, chargingPoint.Address);
+        }
+    }
+}
---
+++ Material Obligatorio/Obligatorio/Ministerio de Turismo/MinTur.Models/In/CharginPointIntentModel.cs
@@ -0,0 +1,29 @@
+﻿using MinTur.Domain.BusinessEntities;
+using System;
+using System.Collections.Generic;
+
+namespace MinTur.Models.In
+{
+    public class ChargingPointIntentModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public int RegionId { get; set; }
+        
+
+        public ChargingPoint ToEntity()
+        {
+            ChargingPoint chargingPoint = new ChargingPoint()
+            {
+                Name = Name,
+                Description = Description,
+                Address = Address,
+                RegionId = RegionId,
+                
+            };
+            return chargingPoint;
+        }
+
+    }
+}
