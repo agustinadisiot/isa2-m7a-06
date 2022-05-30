6353f6832c598a4d5344e7317a1131e1831c7757
Agustina <agustinadisiotvevo@gmail.com>
2022-05-30 19:47:13 -0300

    Adds get all charging points features

---
+++ Material Obligatorio/Obligatorio/Ministerio de Turismo/MinTur.WebApiControllerSpecFlow/Features/GetAllCharginPoint.feature
@@ -0,0 +1,15 @@
+﻿Feature: GetAllChargingPoints
+
+A short summary of the feature
+
+@tag1
+Scenario: Get All Charging Point Correctly
+	Given existing charging points
+	When the user selects button to get all charging points in the lateral menu
+	Then the result should be a list of charging points
+
+Scenario: Get All Charging Points Empty List 
+	Given there are not charging points
+	When the user selects button to get all charging points in the lateral menu
+	Then the result should be an empty list
+
---
+++ Material Obligatorio/Obligatorio/Ministerio de Turismo/MinTur.WebApiControllerSpecFlow/StepDefinitions/GetAllChargingPointsStepDefinition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace MinTur.WebApiControllerSpecFlow.StepDefinitions
+{
+    [Binding]
+    public class GetAllChargingPointsStepDefinition
+    {
+        [Given(@"there are not charging points")]
+        public void GivenThereAreNotChargingPoints()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user selects button to get all charging points in the lateral menu")]
+        public void WhenTheUserSelectsButtonToGetAllChargingPointsInTheLateralMenu()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
