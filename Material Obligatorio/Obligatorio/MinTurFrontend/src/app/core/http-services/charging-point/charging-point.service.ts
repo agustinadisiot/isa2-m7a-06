24bc5d9090a032120d9dc4c2b60bd8ea3938a9da
Agustina <agustinadisiotvevo@gmail.com>
2022-06-04 20:07:13 -0300

    Adds tests

--- Material Obligatorio/Obligatorio/MinTurFrontend/src/app/core/http-services/charging-point/charging-point.service.ts
+++ Material Obligatorio/Obligatorio/MinTurFrontend/src/app/core/http-services/charging-point/charging-point.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ChargingPointBasicInfoModel} from "../../../shared/models/out/charging-point-basic-info-model";
-import { ChargingPointIntentModel } from 'src/app/shared/models/out/charging-point-intent-model';
+import { ChargingPointIntentModel } from '../../../src/app/shared/models/out/charging-point-intent-model';
 
 
 @Injectable({
